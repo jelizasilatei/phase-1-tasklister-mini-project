14ca091c86a4d2232046eb74fd28a780c9b5a3b3
jelizasilatei <silateijc90@gmail.com>
2023-04-02 19:56:37 +0300

    Can type a task Submit the task See the string task on our DOM Delete a task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form');
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      buildWork(e.target.work.value);
+      form.reset();
+  })
+})
+
+function buildWork(work) {
+  let list = document.createElement('li');
+  const btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = 'Delete';
+  btn.style.width = '40px';
+  btn.style.height = '30px';
+  btn.style.fontSize ='10px';
+  list.textContent = `${work}  `;
+  document.querySelector("#tasks").appendChild(list);
+  list.appendChild(btn);
+  console.log(list);
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove(); 
+}
